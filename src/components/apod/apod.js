3a7f00f7b040c3db36641c87a5374160be77d404
Daniel Rojo <danielrojo@gmail.com>
2021-09-09 16:16:39 +0200

    apod component with date picker

--- src/components/apod/apod.js
+++ src/components/apod/apod.js
@@ -1,4 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import moment from 'moment';
+import DatePicker from "react-datepicker";
+import YouTube from 'react-youtube';
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class Apod extends Component {
 
@@ -8,6 +12,14 @@ export default class Apod extends Component {
         this.state = {
             apod: {}
         }
+
+        this.opts = {
+            height: '390',
+            width: '640',
+            playerVars: {
+              autoplay: 0,
+            }
+        }
     }
 
     componentDidMount() {
@@ -15,18 +27,43 @@ export default class Apod extends Component {
             .then(response => response.json())
             .then(data => {
                 this.setState({
-                    apod: data
+                    apod: data,
+                    date: new Date()
                 })
             })
     }
 
+    setDate(date) {
+        fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=' + moment(date).format('YYYY-MM-DD'))
+        .then(response => response.json())
+        .then(data => {
+            this.setState({
+                apod: data,
+                date: date
+            })
+        })
+
+    };
+        
     render() {
+
+        let media = <div></div>;
+
+        if(this.state.apod.media_type === 'image') {
+            media = <img class="d-block mx-auto h-100" src={this.state.apod.url} alt={this.state.apod.title} />
+        }
+        else if(this.state.apod.media_type === 'video') {
+            console.log(this.state.apod.url.split('/')[4]);
+            media = <YouTube class="d-block mx-auto" videoId={this.state.apod.url.split('/')[4]} opts={this.opts} />
+        }
+
         return (
             <div class="container">
+                <DatePicker selected={this.state.date} onChange={(date) => {this.setDate(date)}} />
                 <div class="jumbotron">
                     <h1 class="display-3">{this.state.apod.title}</h1>
                     <p class="lead">{this.state.apod.date}</p>
-                    <img class="d-block mx-auto h-100" src={this.state.apod.url} alt={this.state.apod.title}/>
+                    {media}
                     <hr class="my-2" />
                     <p>{this.state.apod.explanation}</p>
                     <p class="lead">
